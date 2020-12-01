232d98a081c7bfaa37541270e391683d68b386f0
LemonQiu <18555014012@163.com>
2020-12-02 03:29:00 +0800

    不使用lamdba表达式

--- src/main/java/org/example/socket/v1/ServerSocket4Properties.java
+++ src/main/java/org/example/socket/v1/ServerSocket4Properties.java
@@ -68,26 +68,29 @@ public class ServerSocket4Properties {
                 client.setSoTimeout(CLI_TIMEOUT);
                 client.setTcpNoDelay(CLI_NO_DELAY);
 
-                new Thread(() -> {
-                    try {
-                        System.out.println("client: " + client);
-                        InputStream inputStream = client.getInputStream();
-                        while (true) {
-                            byte[] bytes = new byte[4096];
-                            // read()方法也是阻塞的
-                            int size = inputStream.read(bytes);
-                            if(size < 0) {
-                                client.close();
-                                System.out.println("client closed.....");
-                                break;
-                            } else if(size == 0) {
-                                break;
-                            } else {
-                                System.out.println("client send msg: " + new String(bytes, 0, size, Charset.defaultCharset()));
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            System.out.println("client: " + client);
+                            InputStream inputStream = client.getInputStream();
+                            while (true) {
+                                byte[] bytes = new byte[4096];
+                                // read()方法也是阻塞的
+                                int size = inputStream.read(bytes);
+                                if(size < 0) {
+                                    client.close();
+                                    System.out.println("client closed.....");
+                                    break;
+                                } else if(size == 0) {
+                                    break;
+                                } else {
+                                    System.out.println("client send msg: " + new String(bytes, 0, size, Charset.defaultCharset()));
+                                }
                             }
+                        } catch (IOException e) {
+                            System.out.println("IOException e:" + e.toString());
                         }
-                    } catch (IOException e) {
-                        System.out.println("IOException e:" + e.toString());
                     }
                 }).start();
             }
